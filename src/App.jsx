4c813105cc3658d1f9348a7cec62c774b2c36052
Dmitrienko Artem <admitienko@mail.ru>
2024-06-30 13:11:49 +0300

    Initinal commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { Stars } from "./components/Stars/Stars.jsx";
+
+function App() {
+    return (
+        <Stars count={4}></Stars>
+    );
+}
+export default App
---
+++ src/components/Stars/Stars.jsx
@@ -0,0 +1,20 @@
+import { Star } from "./Star/Star.jsx";
+import './Stars.css'
+import { nanoid } from 'nanoid'
+
+export function Stars({ count= 0 }) {
+    if ( count < 1 || count > 5 || typeof count !== 'number') {
+        return null
+    } else {
+        const items = [];
+        for (let i = 1; i <= count; i += 1) {
+            items.push(<li key={nanoid()}><Star /></li>);
+        }
+        return (
+            <ul className="card-body-stars u-clearfix">
+                {items}
+            </ul>
+        )
+    }
+
+}
